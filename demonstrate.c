4c7a0811e96cb5bf96c2f020d103f81c36514ccd
Shimu13-code <jarin.shimu@auw.edu.bd>
2026-02-07 23:33:46 +0600

    Add files via upload

---
+++ demonstrate.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 10;
+    int b = 10;
+
+    a++; // a becomes 11
+    b--; // b becomes 9
+
+    printf("Incremented a: %d\n", a);
+    printf("Decremented b: %d\n", b);
+}
